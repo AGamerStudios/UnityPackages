19f3b553ecc030e42658ca663432584158997701
Amos Garner <aagarner.7@gmail.com>
2019-04-01 14:06:20 -0400

    Implement Audio Manager

---
+++ src/AudioManager/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public List<AudioClip> clips = new List<AudioClip>();
+    public GameObject AudioSourcePrefab;
+
+    public void PlaySound(string title, bool selfDestruct)
+    {
+        AudioClip clip = clips.Find(item => item.name.ToLower() == title.ToLower());
+        SpawnAudioSource(clip, selfDestruct);
+    }
+
+    private void SpawnAudioSource(AudioClip clip, bool selfDestruct)
+    {
+        GameObject entity = Instantiate(AudioSourcePrefab, Vector3.zero, Quaternion.identity);
+        entity.transform.parent = transform;
+        AudioSource source = entity.GetComponent<AudioSource>();
+        source.clip = clip;
+        entity.GetComponent<AudioSourceController>().selfDestruct = selfDestruct;
+    }
+}
---
+++ src/AudioManager/Scripts/AudioSourceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceController : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Determines if audio source will self destruct")]
+    public bool selfDestruct = true;
+    protected float lifeTime, startTime;
+    
+    void Start()
+    {
+        SetEntityName();
+        SetStartTime();
+        SetSourceLifeTime();
+        GetComponent<AudioSource>().Play();
+    }
+    void Update()
+    {
+        if(selfDestruct){
+            DestroyOnEndOfLifetime();
+        }
+    }
+
+    protected void SetEntityName()
+    {
+        gameObject.name = GetComponent<AudioSource>().clip.name;
+    }
+    protected void SetStartTime()
+    {
+        startTime = Time.time;
+    }
+    protected void SetSourceLifeTime()
+    {
+        lifeTime = GetComponent<AudioSource>().clip.length;
+    }
+    protected void DestroyOnEndOfLifetime()
+    {
+        if (Time.time - startTime > lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
