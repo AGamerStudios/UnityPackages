521fb93e124c0d8fa5b13c28319bffb74167345c
Amos Garner <aagarner.7@gmail.com>
2019-04-02 19:01:26 -0400

    Implement Input Manager Plugin

---
+++ src/InputManager/Scripts/InputAnimationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InputAnimationController : MonoBehaviour
+{
+    public bool isHovering = false;
+    public bool isShaking = false;
+    public string value = " ";
+    private TMP_Text valueText;
+    private Animator animator;
+    private Vector3 startPosition;
+    private Quaternion startRotation;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        startRotation = transform.rotation;
+        animator = GetComponent<Animator>();
+        valueText = GetComponentInChildren<TMP_Text>();
+        valueText.text = value;
+    }
+
+    void Update()
+    {
+        UpdateAnimatorBools();
+        
+        if (isHovering)
+        {
+            isShaking = false;
+        }
+        if (isShaking)
+        {
+            isHovering = false;
+        }
+
+        if (!animator.GetBool("IsShaking"))
+        {
+            ResetEntityRotation();
+        }
+    }
+
+    private void UpdateAnimatorBools()
+    {
+        animator.SetBool("IsHovering", isHovering);
+        animator.SetBool("IsShaking", isShaking);
+    }
+
+    private void ResetEntityRotation()
+    {
+        transform.rotation = startRotation;
+    }
+}
---
+++ src/InputManager/Scripts/InputManagerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManagerController : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        HandleButtonInputs();
+    }
+
+    /*
+    * Handles Unity's Button Inputs. 
+    * This will need to be configure for every game.
+     */
+    private void HandleButtonInputs()
+    {
+        if(Input.GetButtonDown("Escape")){
+            Application.Quit();
+        }
+    }
+}
